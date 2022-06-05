6cd3475444529a7399c08977d9dae48a9568b2b2
monicaliz <monicaeliz3@gmail.com>
2022-06-04 20:57:56 -0400

    commit tree ae96631116981bbe36f5964bf42a2113e7f5f5bb

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+//initialize employee object
+const employee = {
+    name: "mark",
+    streetAddress: "123 lane",
+}
+
+//updateEmployeeWithKeyAndValue() -- spread operator
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = { ... employee }
+    newObj [key] = value
+    return newObj
+}
+
+//destructivelyUpdateEmployeeWithKeyAndValue()
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,  key, value) {
+    employee [key] = value
+     return employee
+}
+
+//deleteFromEmployeeByKey() == spread operator
+function deleteFromEmployeeByKey(employee,  key, value) {
+    const employeeNew = {...employee}
+    employeeNew [key] = value
+    return employeeNew
+}
+
+//destructivelyDeleteFromEmployeeByKey()
+function destructivelyDeleteFromEmployeeByKey(employee,  key, value) {
+    delete employee [key] 
+    return employee
+}
